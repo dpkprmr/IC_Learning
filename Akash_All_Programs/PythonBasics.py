e176e192e208d9a8a1da597c807049aa4670a685
Akash Gupta <guptaak@INTEGRICHAIN.LOCAL>
2020-10-05 12:42:36 +0530

    05102020v2

--- Akash_All_Programs/PythonBasics.py
+++ Akash_All_Programs/PythonBasics.py
@@ -39,6 +39,7 @@ print(Task[1:3])
 print(Task*2)
 print(Task+Task2)
 print('Akash Changes')
+print('Chnages')
 
 #tuple inside a List
 Games = [('Criket', 'Football', 'Basketball'), ('2011', '2010', '2008')]
